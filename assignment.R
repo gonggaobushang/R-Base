1149954fb818ad3b42fa97f645160b1659af3d6d
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-07-10 13:24:39 +0800

    Update assignment.R

--- assignment.R
+++ assignment.R
@@ -4,3 +4,7 @@ data=c(-12:12)
 ifelse(data< -5,"YES","NO")
 #不空格的话会出错
 ifelse(data<-5,"YES","NO")
+#替换factor格式的内容
+factor(iris$Species,labels = c(1,2,3))
+#可以批量生成变量
+assign(x=paste0("a2",3),value = iris$Sepal.Length)
