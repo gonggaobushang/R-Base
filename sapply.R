a54b07c94edb760c2a8493ea70bc7df28606cfc6
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-10-31 16:47:37 +0800

    Add files via upload

---
+++ sapply.R
@@ -0,0 +1,30 @@
+data(quakes)
+getColor <- function(quakes) {
+  sapply(quakes$mag, function(mag) {
+    if(mag <= 4) {
+      "green"
+    } else if(mag <= 5) {
+      "orange"
+    } else {
+      "red"
+    } } )
+}
+getColor(quakes[c(1:20),])
+
+
+data(iris)
+
+irisiri<-function(iris){
+  sapply(iris$Species,function(Specise){
+    if(Specise=="setosa" ){
+      "1"
+    }else if(Specise=="versicolor"){
+      "2"
+    }else{
+      3
+    }
+  }
+         )
+}
+irisiri(iris[c(1:12),])
+
